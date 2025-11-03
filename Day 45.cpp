d435554d667cea1654ac000a1851821d37d37500
Piyush99917 <pbhunia521@gmail.com>
2025-11-03 06:26:47 +0530

    Add files via upload

---
+++ Day 45.cpp
@@ -0,0 +1,12 @@
+/*Write a program to convert temperature from Celsius to Fahrenheit.*/
+#include<stdio.h>
+int main()
+{
+    float temp=0,fahren=0,num=1.8;
+    //fahren=(temp*num)+32
+    printf("enter the temperature");
+    scanf("%f",&temp);
+    fahren=(temp*num)+32;
+    printf("temperature in fahrenheit is=%f\n",fahren);
+    return 0;
+}
---
+++ Day 94.cpp
@@ -0,0 +1,17 @@
+/*Write a program to input an integer and check whether it is even or odd using if–else.*/
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("enter an integer:");
+    scanf("%d",&num);
+    if(num%2==0)
+    {
+        printf("the number is even");
+    }
+    else
+    {
+        printf("the number is odd");
+    }
+    return 0;
+}
